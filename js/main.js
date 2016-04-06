196cae4ad4852827d04eb1ba548483aa37c86798
Irina Kashyntseva <i.kashyntseva@gmail.com>
2016-04-06 22:04:26 +0200

    bugs after check #1

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
         if (el.classList) {
           el.classList.toggle(className);
         } else {
-          var classes = el.className.split(' ');
+          var classes = el.className.split(" ");
           var existingIndex = classes.indexOf(className);
 
           if (existingIndex >= 0)
@@ -11,18 +11,38 @@
           else
             classes.push(className);
 
-          el.className = classes.join(' ');
+          el.className = classes.join(" ");
         }
     }
+    function removeClass(el, className) {
+        if (el.classList)
+          el.classList.remove(className);
+        else
+          el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ')
+    }
     function menuToggle(event) {
-        var menuDisplay = document.querySelector('.menu').style.display;
-        toggleClass(document.querySelector('.menu__button'), 'menu__button--close');
-        menuDisplay == '' ||  menuDisplay == 'none' ? menuDisplay = 'block' : menuDisplay = 'none';
-        document.querySelector('.menu').style.display =  menuDisplay;
+        var menuDisplay = document.querySelector(".menu").style.display;
+        toggleClass(document.querySelector(".menu__button"), "menu__button--close");
+        menuDisplay == "" ||  menuDisplay == "none" ? menuDisplay = "block" : menuDisplay = "none";
+        document.querySelector(".menu").style.display =  menuDisplay;
+    }
+    var clientWidth = document.body.clientWidth;
+    var menuButton =  document.querySelector(".menu__button");
+    var menu = document.querySelector(".menu");
+    var header = document.querySelector(".page-header");
+    window.onload = function() {
+       if(menu) {
+           if((menu.className.indexOf("menu--open") !==1) && (menuButton.className.indexOf("menu__button--close") !==1)) {
+               removeClass(menu, "menu--open");
+               toggleClass(menuButton, "menu__button--close");
+               removeClass(header, "page-header--no-js");
+           }
+       }
     }
 
-    var menuButton =  document.querySelector('.menu__button');
+
     if(menuButton){
-      menuButton.addEventListener('click', menuToggle);
+      menuButton.addEventListener("click", menuToggle);
     }
+
 }());
